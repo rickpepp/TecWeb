77a547921b372e3cc9fbaef626543c281430e6e5
conanmk88 <119083100+conanmk88@users.noreply.github.com>
2023-01-23 03:40:15 +0100

    added old html template and new php template

---
+++ views/categoria.php
@@ -0,0 +1,27 @@
+<?php
+    require_once ("../libs/bootstrap.php");
+    require_once ("../libs/functions.php");
+
+    sec_session_start();
+
+    if ($dbh -> login_check()) {
+        $templateParams["titolo"] = "TinkleArt - Categoria";
+        $templateParams["css"] = "categoria.css";
+        $templateParams["iconaTab"] = "Home.png";
+        $templateParams["iconaMod"] = "Modifica.png";
+        $templateParams["iconaElimina"] = "Elimina.png";
+        
+        //Aside
+        $templateParams["categorie"] = $dbh -> getCategorie(3,$_SESSION['user_id']);
+        $templateParams["following"] = $dbh -> getFollowing($_SESSION['user_id']);
+
+        //Section
+        $templateParams["section"] = "categoria-home.php";
+        $templateParams["mioprofilo"] = $dbh -> getPersona($_SESSION["user_id"]);
+        $templateParams["categoriePost"] = $dbh -> getPostByCategory($_GET["idcategoria"]);
+        require '../libs/base.php';
+
+    } else {
+        header('location: ../views/login.php');
+    }
+?>
---
+++ views/registrati.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="it" class="one_column_everywhere">
+    <head>
+        <title>TinkleArt - Registrati</title>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <link rel="stylesheet" href="../css/login.css"/>
+        <link rel="icon" type="img/png" href="../img/icone/Amici.png" />
+        <script type="text/javascript" src="../js/sha512.js"></script>
+        <script type="text/javascript" src="../js/forms_registrazione.js"></script>
+    </head>
+    <body>
+        <header>
+            <img src="../img/icone/TinkleArt.png" alt="TinkleArt">
+        </header>
+        <main>
+            <div>
+                <form action="../libs/processo_registrazione.php" method="post" name="signup_form">
+                    <h2>Crea un nuovo account</h2><br>
+                    <label>Nome:<input type="text" name="nome_registrazione"></label><br>
+                    <label>Cognome:<input type="text" name="cognome_registrazione"></label><br>
+                    <label>Email:<input type="text" name="email_registrazione"></label><br>
+                    <label>Password:<input type="password" name="password_registrazione" id="spassword" onkeyup="controllo_password()"></label><br>
+                    <label>Conferma Password:<input type="password" name="cpassword_registrazione" id="check_password" onkeyup="password_uguali()" disabled></label><br>
+                    <footer>
+                        <p id="informazioni"></p>
+                        <input type="button" value="Registrati" class="rosso" id="rbutton" onclick="formhashr(this.form, this.form.spassword,'registrazione');">
+                        <a href="./login.php">Sono già registrato</a>
+                    </footer>
+                </form>
+            </div>
+        </main>
+    </body>
+</html>
