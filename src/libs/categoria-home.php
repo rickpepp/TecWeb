f3ace2cbdbe915e80e8cc321ed95c9cae043b9d1
Riccardo Pepe <riccardo.pepe3@studio.unibo.it>
2023-01-27 01:50:26 +0100

    Correzione bottone segui/non seguire più categoria

--- src/libs/categoria-home.php
+++ src/libs/categoria-home.php
@@ -3,7 +3,7 @@
 <?php else: ?>
 <div class="titolo">
     <h1><?php echo $templateParams["categoria"][0]["nomecategoria"];?> </h1>
-    <input type="<?php echo $templateParams["categoria"][0]["tipoBottone"] ?>" value="<?php echo $templateParams["categoria"][0]["testoBottone"] ?>" onclick="setCategorie(<?php echo $_GET['idcategoria'] ?>)">
+    <input type="<?php echo $templateParams["categoria"][0]["tipoBottone"] ?>" value="<?php echo $templateParams["categoria"][0]["testoBottone"] ?>" onclick="setCategorie(<?php echo $_GET['idcategoria'] ?>)" id="cat_smartphone_<?php echo $_GET['idcategoria'] ?>">
 </div>
 <?php if($templateParams["categoriePost"]==null): ?>
     <p>NESSUN POST TROVATO PER QUESTA CATEGORIA</p>
